9f91fcaafe90de6f896a0e9432496783f2040d9d
aa126237 <alexaanderson2002@gmail.com>
2019-05-17 12:48:36 -0500

    update

---
+++ src/Movement.java
@@ -0,0 +1,21 @@
+public class Movement {
+
+    private static int x = 0;
+    private static int y = 0;
+
+    public static void movementMain(){
+
+
+
+        /*  Inventory
+            Move Up
+            Move Down
+            Move Left
+            Move Right
+            Exit and Save
+            Text
+            Random Generator for encounters
+            Location
+         */
+    }
+}
