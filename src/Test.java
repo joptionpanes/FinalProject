9fe103d18f391e8259ff5fdaef552035d2f5b4b4
Cornelison <DC155708@davenportschools.org>
2019-05-20 13:24:57 -0500

    Map

--- src/Test.java
+++ src/Test.java
@@ -1,18 +1,41 @@
+//https://stackoverflow.com/questions/13707223/how-to-write-an-array-to-a-file-java
+import javax.swing.*;
+import java.io.*;
+
 public class Test {
     public static void main(String[] args){
-        Character player = new Character();
+        File saveLocation = new File(Save.getSaveLocation());
+        if (saveLocation.exists()){
+            System.out.println("Save location already exists!");
+        } else {
+            boolean f = saveLocation.mkdirs(); //attempt to create dir returning boolean (true means it was made)
+            if(f) {
+                System.out.println("Directory Created");
+            } else {
+                JOptionPane.showMessageDialog(null, "Error in creating save file directory! Access is probably denied",
+                    "Error", JOptionPane.ERROR_MESSAGE); //if an error occurred, say so.
+            }
+        }
+        String[] options = {"Start New", "Load Existing"};
 
-        player.addInventory("Item", 5);
-        Object[][] inventory = player.getInventory();
-        System.out.print(inventory[0][0] + ", " + inventory[0][1] + "\n");
-        player.addInventory("Item", 5);
-        System.out.print(inventory[0][0] + ", " + inventory[0][1] + "\n");
-        player.addInventory("Magic Armor", 20);
-        System.out.print(inventory[1][0] + ", " + inventory[1][1] + "\n");
-        player.addInventory("Magic Armor", 40);
-        System.out.print(inventory[1][0] + ", " + inventory[1][1] + "\n");
-        System.out.print(inventory[2][0] + ", " + inventory[2][1] + "\n");
-
-
+        int selection = JOptionPane.showOptionDialog(null, "Welcome to " + Save.getGameName() + ". Select an option:",
+                Save.getGameName(), JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE, null, options, null);
+        if (selection == 0){ //start new
+            JFileChooser selectedFile;
+            do {
+                selectedFile = new JFileChooser(saveLocation); //save dialog opens in saves folder
+            }while (selectedFile.showDialog(null, "Enter name of new save folder") != JFileChooser.APPROVE_OPTION);
+            Save.setSaveLocation(selectedFile.getSelectedFile()); //saveFile = save file location
+            File saveFile = selectedFile.getSelectedFile();
+            File map = new File(saveFile + "\\map");
+            map.mkdirs();
+            Save.setMapLocation();
+            Save.setMapAtPos(0, 0, "City");
+            String y = Save.getMapAtPos(0, 0);
+            System.out.println(y);
+        }
+        else if (selection == 1){ //load existing
+            Object[] save = Save.loadFromFile();
+        }
     }
 }
