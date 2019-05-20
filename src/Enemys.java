c54e4d154e265e1ccd48539a04e0a67b4b1818fa
Ruben <thezombiekiller81@gmail.com>
2019-05-20 13:22:27 -0500

    0

--- src/Enemys.java
+++ src/Enemys.java
@@ -7,51 +7,33 @@ public class Enemys {
     private int armor;
     private String name;
 
+    private int[] MaxHitPointsarray = {(int) (Math.random()*((10-1)+1))+1,(int)(Math.random()*((15-5)+1))+5,(int)
+            (Math.random()*((10-8)+1))+8,(int) (Math.random()*((30-20)+1))+20,(int) (Math.random()*((60-40)+1))+40};
 
-    public void newSlime(){
-        maxHitPoints =(int) (Math.random()*((10-1)+1))+1;
-        Hitpoints = maxHitPoints;
-        attack = (int) (Math.random()*((3-1)+1))+1;
-        xpGiven = (int)(Math.random() *((5-1)+1))+1;
-        goldGiven = (int)(Math.random()*((3))+1);
-        name = "Slime";
+    private int[] AttackArray = {(int) (Math.random()*((3-1)+1))+1, (int) (Math.random()*((45-35)+1))+35, (int)
+            (Math.random()*((30-25)+1))+25,(int) (Math.random()*((50-45)+1))+45, (int) (Math.random()*((50-45)+1))+45};
+
+    private int[] ArmorArray = {0,(int)(Math.random()*((3)+1)),(int)(Math.random()*((2)+1)),(int)(Math.random()*
+            ((5-1)+1))+1,(int)(Math.random()*((8-5)+1))+5};
+
+    private int[] xpGivenArray = {(int)(Math.random() *((5-1)+1))+1,(int)(Math.random() *((7-1)+1))+1,(int)(Math.random() *
+            ((10-3))+1)+3,(int)(Math.random() *((15-8)+1))+8,(int)(Math.random() *((15-6))+1)+6};
+
+    private int[] goldGivenArray = {(int)(Math.random()*((3))+1),(int)(Math.random()*((6-2))+1)+2,(int)(Math.random()*
+            ((7-4))+1)+4,(int)(Math.random()*((16-6))+1)+6,(int)(Math.random()*((20-10))+1)+10};
+
+    private String[] names = {"Slime","Goblin","Undead","Orc","Giant"};
+
+    public void enemyGenerator() {
+        int encounter = (int) (Math.random() * ((5) + 1));
+        int maxHitPoints = MaxHitPointsarray[encounter];
+        int attack = AttackArray[encounter];
+        int xpGiven = xpGivenArray[encounter];
+        int goldGiven = goldGivenArray[encounter];
+        int armor = ArmorArray[encounter];
+        String name = names[encounter];
     }
-    public void newOrc(){
-        maxHitPoints =(int) (Math.random()*((30-20)+1))+20;
-        Hitpoints = maxHitPoints;
-        attack = (int) (Math.random()*((50-45)+1))+45;
-        armor = (int)(Math.random()*((5-1)+1))+1;
-        xpGiven = (int)(Math.random() *((15-8)+1))+8;
-        goldGiven = (int)(Math.random()*((16-6))+1)+6;
-        name = "Orc";
-    }
-    public void newGoblin(){
-        maxHitPoints =(int) (Math.random()*((15-5)+1))+5;
-        Hitpoints = maxHitPoints;
-        attack = (int) (Math.random()*((45-35)+1))+35;
-        armor = (int)(Math.random()*((3)+1));
-        xpGiven = (int)(Math.random() *((7-1)+1))+1;
-        goldGiven = (int)(Math.random()*((6-2))+1)+2;
-        name = "Goblin";
-    }
-    public  void newUndead(){
-        maxHitPoints =(int) (Math.random()*((10-8)+1))+8;
-        Hitpoints = maxHitPoints;
-        attack = (int) (Math.random()*((30-25)+1))+25;
-        armor = (int)(Math.random()*((2)+1));
-        xpGiven = (int)(Math.random() *((10-3))+1)+3;
-        goldGiven = (int)(Math.random()*((7-4))+1)+4;
-        name = "Undead";
-    }
-    public void newGiant(){
-        maxHitPoints =(int) (Math.random()*((60-40)+1))+40;
-        Hitpoints = maxHitPoints;
-        attack = (int) (Math.random()*((50-45)+1))+45;
-        armor = (int)(Math.random()*((8-5)+1))+5;
-        xpGiven = (int)(Math.random() *((15-6))+1)+6;
-        goldGiven = (int)(Math.random()*((20-10))+1)+10;
-        name = "Giant";
-    }
+
     public void newDragon(){
         maxHitPoints=(int) (Math.random()*((100-80)+1))+80;
         Hitpoints = maxHitPoints;
