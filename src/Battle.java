ee8c2b58597af7037b3f383ee8fdab4a86408c15
CodingSyntax <37378044+CodingSyntax@users.noreply.github.com>
2019-05-22 13:42:18 -0500

    Add files via upload

---
+++ src/Battle.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+
+public class Battle {
+    public Battle( Character player, Enemys enemy){
+        enemy.enemyGenerator();
+        while(player.isAlive() && enemy.isAlive()){
+           String[] battleOptions = {"Attack","Items"};
+            int result = JOptionPane.showOptionDialog(null, null, "Choose what to do", JOptionPane.YES_NO_CANCEL_OPTION,
+                    JOptionPane.PLAIN_MESSAGE ,null,battleOptions,null);
+            if (result == 0){
+                enemy.defend(player);
+            }
+            else{
+                //don't know how to call inventory
+            }
+            if (enemy.isAlive()){
+                player.defend(enemy);
+            }
+        }
+    }
+}
