33be66129e9700a45f6e6ffac593b4c1c58c10d7
CodingSyntax <37378044+CodingSyntax@users.noreply.github.com>
2019-05-27 22:30:45 -0500

    Add files via upload

---
+++ src/Movement.java
@@ -0,0 +1,217 @@
+import javax.swing.*;
+
+public class Movement {
+
+    /*      !TO DO BEFORE FINiSHED!          *
+     * 1.Add in inventory                     *
+     * 2.Add a way to exit and save           *
+     * 3.Send encounters to the right methods *
+     *                                        *
+     * -Alex Anderson                         *
+     *****************************************/
+
+    //VARIABLES
+    private static int x = 0;
+    private static int y = 0;
+    private static int[] location = { x, y };
+    final private static int[] DRAGON_LAIR = {100, 100};
+    private static int moveRand;
+    private static int moveRand1;
+    private static int moveRand2;
+    private static int movesBeforeEncounter = 1;
+    private static int max = 10;
+    private static int min = movesBeforeEncounter;
+    private static int range = max - movesBeforeEncounter + 1;
+    private static int choice = 1;
+
+
+    //MOVEMENT MAIN METHOD
+    public static void movementMain(){
+        //Making The Control Panel for Movement
+        String[] options = new String[] {"Move North", "Move South", "Move East", "Move West",
+                "Inventory", "Save", "Exit and Save"};
+        int response = JOptionPane.showOptionDialog(null, "Control Panel", "Movement",
+                JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE,
+                null, options, options[0]);
+
+        JPanel panel2 = new JPanel();
+        JTextField textField = new JTextField(10);
+        panel2.add(textField);
+
+        if (response == 0)
+            moveNorth();
+        else if(response == 1)
+            moveSouth();
+        else if(response == 2)
+            moveEast();
+        else if(response == 3)
+            moveWest();
+        else if(response == 4)
+            Start.player.displayInv();
+        else if (response == 5)
+            save();
+        else if(response == 6 || response == -1)
+            exitAndSave();
+
+        location[0] = x;
+        location[0] = y;
+    }
+
+
+    //MOVEMENT METHODS
+    public static void moveNorth(){
+        y += 1;
+        System.out.println("Moving North to: " + x + ", " + y);
+        if (checkForExistingCity() != 0) {
+            movesBeforeEncounter();
+            moveRand1 = (int) (Math.random() * range) + min;
+            moveRand2 = (int) (Math.random() * range) + min;
+            if (moveRand1 == moveRand2) {
+                encounter();
+            } else
+                Save.setMapAtPos(x, y, "NULL", null);
+        }
+    }
+
+    public static void moveWest(){
+        x -= 1;
+        System.out.println("Moving West to: " + x + ", " + y);
+        if (checkForExistingCity() != 0) {
+            movesBeforeEncounter();
+            moveRand1 = (int) (Math.random() * range) + min;
+            moveRand2 = (int) (Math.random() * range) + min;
+            if (moveRand1 == moveRand2) {
+                encounter();
+            } else
+                Save.setMapAtPos(x, y, "NULL", null);
+        } else {
+            Encounters encounter = new Encounters();
+            encounter.loadCity();
+        }
+    }
+
+    public static void moveEast(){
+        x += 1;
+        System.out.println("Moving East to: " + x + ", " + y);
+        if (checkForExistingCity() != 0) {
+            movesBeforeEncounter();
+            moveRand1 = (int) (Math.random() * range) + min;
+            moveRand2 = (int) (Math.random() * range) + min;
+            if (moveRand1 == moveRand2) {
+                encounter();
+            } else
+                Save.setMapAtPos(x, y, "NULL", null);
+        }
+    }
+
+    public static void moveSouth(){
+        y -= 1;
+        System.out.println("Moving South to: " + x + ", " + y);
+        if (checkForExistingCity() != 0) {
+            movesBeforeEncounter();
+            moveRand1 = (int) (Math.random() * range) + min;
+            moveRand2 = (int) (Math.random() * range) + min;
+            if (moveRand1 == moveRand2) {
+                encounter();
+            } else
+                Save.setMapAtPos(x, y, "NULL", null);
+        }
+    }
+
+
+    //ENCOUNTER METHODS
+    public static void movesBeforeEncounter(){
+        movesBeforeEncounter++;
+        range = max - movesBeforeEncounter + 1;
+    }
+
+    public static void encounter(){
+        movesBeforeEncounter = 0; //chances 10 30 60
+        moveRand = (int)(Math.random() * 10) + 1;
+        Encounters encounter = new Encounters();
+        if(moveRand <= 1){
+            System.out.println("Wander encounter");
+            Save.setMapAtPos(x, y, "NULL", null);
+            encounter.generateWandererShop();
+        }
+        else if((moveRand >= 7) && checkForExistingCity() == -1){
+            System.out.println("City encounter");
+            encounter.generateCity();
+        }
+        else{
+            System.out.println("Enemy encounter");
+            Save.setMapAtPos(x, y, "NULL", null);
+            encounter.generateBattle();
+        }
+    }
+
+    public static void caveEncounter(){
+        if (location == DRAGON_LAIR);
+        //dragonsLair();
+    }
+
+    public static int checkForExistingCity(){ //0 = does exist, 1 = doesn't exist, -1 = not generated
+        Object[][] save = Save.getMapAtPos(x, y);
+        if (save == null) {
+            return -1;
+        } else if (save[0][0].equals("NULL")){
+            return 1;
+        } else {
+            Encounters encounter = new Encounters();
+            encounter.loadCity();
+            return 0;
+        }
+    }
+
+
+    //EXIT AND SAVE METHODS
+    public static void exitAndSave(){
+        Start.player.save();
+        System.exit(3);
+    }
+
+    public static void save(){
+        Start.player.save();
+    }
+
+
+    //SETTER METHODS
+    public static void setX(int x1){ x = x1; }
+
+    public static void setY(int y1){ y = y1; }
+
+    public static void setMoveRand1(){ moveRand1 = (int)(Math.random() * range) + min; }
+
+    public static void setMoveRand2(){ moveRand2 = (int)(Math.random() * range) + min; }
+
+    public static void setMovesBeforeEncounter(int movesBeforeEncounter1){ movesBeforeEncounter = movesBeforeEncounter1; }
+
+    public static void setMax(int max1){ max = max1; }
+
+    public static void setMin(int min1){ min = min1; }
+
+    public static void setChoice(int choice1){ choice = choice1; }
+
+
+
+    //GETTER METHODS
+    public static int getX(){ return x; }
+
+    public static int getY(){ return y; }
+
+    public static int[] getLocation(){ return location; }
+
+    public static int getMoveRand1(){ return moveRand1; }
+
+    public static int getMoveRand2(){ return moveRand2; }
+
+    public static int getMovesBeforeEncounter(){ return movesBeforeEncounter; }
+
+    public static int getMax(){ return max; }
+
+    public static int getMin(){ return min; }
+
+    public static int getRange(){ return range; }
+
+    public static int getChoice(){ return choice; }
+}
---
+++ src/Start.java
@@ -0,0 +1,61 @@
+import javax.swing.*;
+import java.io.*;
+
+public class Start {
+    public static Character player = new Character();
+    public static void main(String[] args){
+        File saveLocation = new File(Save.getSaveLocation());
+        if (saveLocation.exists()){
+            System.out.println("Save location already exists!");
+        } else {
+            boolean f = saveLocation.mkdirs(); //attempt to create dir returning boolean (true means it was made)
+            if(f) {
+                System.out.println("Directory Created");
+            } else {
+                JOptionPane.showMessageDialog(null, "Error in creating save file directory! Access is probably denied",
+                        "Error", JOptionPane.ERROR_MESSAGE); //if an error occurred, say so.
+            }
+        }
+
+        String[] options = {"Start New", "Load Existing"};
+        int selection = JOptionPane.showOptionDialog(null, "Welcome to " + Save.getGameName() + ". Select an option:",
+                Save.getGameName(), JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE, null, options, null);
+        if (selection == -1){ //if user clicked x
+            System.exit(3);
+        }
+        if (selection == 0){ //start new
+            JFileChooser selectFile = new JFileChooser(saveLocation); //save dialog opens in saves folder
+            int selectValue = selectFile.showDialog(null, "Enter name of new save folder");
+            if (selectValue != JFileChooser.APPROVE_OPTION){ //if user selects x or cancel
+                main(args);
+                System.exit(2);
+            }
+            Save.setSaveLocation(selectFile.getSelectedFile());
+            File saveFile = selectFile.getSelectedFile(); //saveFile = save file location
+            File map = new File(saveFile + "\\map");
+            map.mkdirs(); //make the map folder
+            Save.setMapLocation(); //set map folder location to newly created map folder
+
+            //class selection
+            String[] classes = player.getClasses();
+            int classChoice;
+            do {
+                classChoice = JOptionPane.showOptionDialog(null, "Choose your class.\nThey do more damage with their " +
+                                "respective weapons", "Class selection", JOptionPane.YES_NO_CANCEL_OPTION,
+                        JOptionPane.PLAIN_MESSAGE, null, classes, null);
+            }while(classChoice==-1);
+            player.setPlayerClass(classChoice);
+            Save.setMapAtPos(0, 0, "NULL", null);
+            player.save();
+        }
+        else if (selection == 1){ //load existing
+            if (!player.load()){ //if user cancelled or clicked x
+                main(args);
+                System.exit(2);
+            }
+        }
+        while (true) {
+            Movement.movementMain();
+        }
+    }
+}
